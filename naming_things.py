a1481fe055d82eecbbe45f6b18dca9a2b58b0a95
Ramisa <ramisa.fayez@my.jcu.edu.au>
2016-11-29 22:42:22 +1000

    Initial commit
    I already wrote this, I just need to keep it somewhere

---
+++ naming_things.py
@@ -0,0 +1,24 @@
+import csv
+
+
+def main():
+    names = load_names()
+    write_script(names)
+
+
+def load_names():
+    old_names = open("oldnames.csv", "r")
+    delimit_file = csv.reader(old_names, delimiter=",")
+    stored_names = []
+    for line in delimit_file:
+        stored_names.append(line)
+    return stored_names
+
+
+def write_script(names):
+    write_new_names = open("thescript.txt", "w")
+    for name in names:
+        print("Get -Mail \"{}\" -Display \"{}\" -IDK".format(name[0], name[1]), file=write_new_names)
+    write_new_names.close()
+
+main()
